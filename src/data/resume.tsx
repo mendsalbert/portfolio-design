47c90bfef59279724c88b8a88f0f3e5d0b9b40f9
mendsalbert <mendsalbert@gmail.com>
2024-08-10 21:14:31 +0000

    portfolio website

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,7 +2,7 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "Dillion Verma",
+  name: "John Doe",
   initials: "DV",
   url: "https://dillion.io",
   location: "San Francisco, CA",
