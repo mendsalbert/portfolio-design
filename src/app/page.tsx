13643bab2cae6cda39fd47a84b5086bf5881f253
K S Saivaibhav <ks.saivaibhav@accolitedigital.com>
2024-09-10 11:33:43 +0530

    initial changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,7 @@ export default function Page() {
                 delay={BLUR_FADE_DELAY}
                 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
                 yOffset={8}
-                text={`Hi, I'm ${DATA.name.split(" ")[0]} 👋`}
+                text={`Hi, I'm Sai 👋`}
               />
               <BlurFadeText
                 className="max-w-[600px] md:text-xl"
--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -2,7 +2,7 @@ import { Icons } from "@/components/icons";
 import { HomeIcon, NotebookIcon } from "lucide-react";
 
 export const DATA = {
-  name: "John Doe",
+  name: "K S Saivaibhav",
   initials: "DV",
   url: "https://dillion.io",
   location: "San Francisco, CA",
